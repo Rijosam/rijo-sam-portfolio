e9797216badd81c4113a574d3f9675483b7a25c6
rijosam <rijosam19@gmail.com>
2025-05-28 13:42:04 +0200

    update socials

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,4 +1,4 @@
 export const LINKEDIN_URL = "https://www.linkedin.com/in/rijosam19/";
 export const GITHUB_URL = "https://github.com/RijoSam";
-export const STRAVA_URL = "https://github.com/RijoSam";
-export const BLUESKY_URL = "https://github.com/RijoSam";
+export const STRAVA_URL = "https://www.strava.com/athletes/34473519";
+export const BLUESKY_URL = "https://bsky.app/profile/rijosam.com";
