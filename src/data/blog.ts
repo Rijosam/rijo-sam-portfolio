dc16e733837c7ff9e91db50ab4430e19010a37bd
rijosam <rijosam19@gmail.com>
2025-08-06 15:45:54 +0200

    Add a new podcast

--- src/data/blog.ts
+++ src/data/blog.ts
@@ -12,13 +12,18 @@ export const Content: BlogContent[] = [
 		url: "https://foojay.io/today/pseudorandom-number-generator/",
 	},
 	{
-		title: "As a developer, how do we keep our body and mind healthy?",
+		title: "Foojay Podcast : As a developer, how do we keep our body and mind healthy?",
 		type: "Podcast",
 		url: "https://www.youtube.com/watch?v=OYX_X2x3vl8&t=1498s",
 	},
 	{
-		title: "Let's Talk About Java Code! Diving into Foojay blog posts",
+		title: "Foojay Podcast : Let's Talk About Java Code! Diving into Foojay blog posts",
 		type: "Podcast",
 		url: "https://www.youtube.com/watch?v=IB7drozkKUk&t=1673s",
+	},
+	{
+		title: "Foojay Podcast : DevBcn Report, Part 1 – Learn from the Community",
+		type: "Podcast",
+		url: "https://youtu.be/LopozI76KyA?feature=shared&t=3975",
 	},
 ];
